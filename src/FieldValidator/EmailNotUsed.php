27866cb0dd6c43f3fd2a7c13b0a26309b3ba4070
Michał Frankiewicz <michal.frankiewicz@wiosna.org.pl>
2016-06-08 15:19:46 +0200

    dodaję walidator sprawdzający czy ktoś się już zgłosił na podanego maila

--- src/FieldValidator/EmailNotUsed.php
+++ src/FieldValidator/EmailNotUsed.php
@@ -2,25 +2,25 @@
 namespace WioForms\FieldValidator;
 
 use SuperW\Repository\Recruitment as RecruitmentRepository;
+use WioForms\FieldConverter\Stanowisko2Role;
+
 
 class EmailNotUsed extends AbstractFieldValidator
 {
 
     public function validatePHP($value, $settings)
     {
-        global $queryBuilder;
-        $recruitmentRepository = new RecruitmentRepository($queryBuilder);
-var_dump($this->wioForms);die;
-        if (is_null($recruitmentRepository->getFirstByEmailAndWioFlowId($value, 12))) {
-            $this->valid = true;
-        }
-        var_dump($test);die;
-        $this->invalidMessage = 'email_used';
+        $this->invalidMessage = 'email_already_used';
 
-        if (filter_var($value, FILTER_VALIDATE_EMAIL))
-        {
-            $this->valid = true;
-        }
+        global $queryBuilder;
+        $recruitmentRepository  = new RecruitmentRepository($queryBuilder);
+        $roleConverter          = new Stanowisko2Role();
+
+        $role       = $roleConverter->convert($this->wioForms->entryData['stanowisko']);
+        $program    = $this->wioForms->entryData['akcja'];
+
+        $this->valid = !$recruitmentRepository
+                            ->ifEmailAlreadyUsed($value, $program, $role);
 
         $this->setAnswer();
         return $this->getReturn();
