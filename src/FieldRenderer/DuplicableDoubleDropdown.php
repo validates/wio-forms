95cf989784a005c47cc302abf0aad4c8257ca653
ViniTou <vinniczek@gmail.com>
2016-06-27 11:38:56 -0400

    Applied fixes from StyleCI

--- src/FieldRenderer/DuplicableDoubleDropdown.php
+++ src/FieldRenderer/DuplicableDoubleDropdown.php
@@ -17,7 +17,7 @@ class DuplicableDoubleDropdown extends AbstractFieldRenderer
 
         $this->duplicateCounter = 0;
 
-        $valueTab = explode('|',$this->value);
+        $valueTab = explode('|', $this->value);
 
         foreach ($valueTab as $key => $value) {
             if ($value == '') {
@@ -26,43 +26,42 @@ class DuplicableDoubleDropdown extends AbstractFieldRenderer
         }
 
         foreach ($valueTab as $programRoleName) {
-            list($programName) = explode(' - ',$programRoleName);
+            list($programName) = explode(' - ', $programRoleName);
 
-            $this->html.= '<b>Program:</b> <br/> <select name="" class="DuplicableDD_level1">';
-            $this->html.= '<option value="">wybierz</option>';
-            $level1=1;
+            $this->html .= '<b>Program:</b> <br/> <select name="" class="DuplicableDD_level1">';
+            $this->html .= '<option value="">wybierz</option>';
+            $level1 = 1;
             foreach ($this->dataSet as $option => $optionData) {
-                $this->html.='<option toChange="DuplicableDD_'.$this->duplicateCounter.'" toShow="DuplicableDD_'.$this->duplicateCounter.'_'.$level1.'" value="option'.($level1++).'"';
+                $this->html .= '<option toChange="DuplicableDD_'.$this->duplicateCounter.'" toShow="DuplicableDD_'.$this->duplicateCounter.'_'.$level1.'" value="option'.($level1++).'"';
                 if ($option == $programName) {
-                    $this->html.= ' selected="selected"';
+                    $this->html .= ' selected="selected"';
                 }
-                $this->html.='>'.$option.'</option>';
+                $this->html .= '>'.$option.'</option>';
             }
-            $this->html.= '</select><br/>';
+            $this->html .= '</select><br/>';
 
-            $this->html.= '<b>Rola:</b> <br/> <select name="'.$this->fieldName.'[]" class="DuplicableDD_'.$this->duplicateCounter.' DuplicableDD_level2">';
-            $level1=1;
+            $this->html .= '<b>Rola:</b> <br/> <select name="'.$this->fieldName.'[]" class="DuplicableDD_'.$this->duplicateCounter.' DuplicableDD_level2">';
+            $level1 = 1;
             foreach ($this->dataSet as $option => $optionData) {
-
-                $this->html.= '<option class="DuplicableDD_'.$this->duplicateCounter.'_'.$level1.'" value="">wybierz</option>';
+                $this->html .= '<option class="DuplicableDD_'.$this->duplicateCounter.'_'.$level1.'" value="">wybierz</option>';
                 foreach ($optionData as $data) {
-
-                  $displayNone = ' style="display: none;"';
-                  if ($option == $programName) {
-                      $displayNone = '';
-                  }
-                  $this->html.='>'.$option.'</option>';
-
-                    $this->html.='<option class="DuplicableDD_'.$this->duplicateCounter.'_'.$level1.'" value="'.$option.' - '.$data.'"'; $this->html.='.$displayNone.';
-                    if ($option.' - '.$data == $programRoleName) {
-                        $this->html.=' selected="selected"';
+                    $displayNone = ' style="display: none;"';
+                    if ($option == $programName) {
+                        $displayNone = '';
                     }
-                    $this->html.='>'.$data.'</option>';
+                    $this->html .= '>'.$option.'</option>';
+
+                    $this->html .= '<option class="DuplicableDD_'.$this->duplicateCounter.'_'.$level1.'" value="'.$option.' - '.$data.'"';
+                    $this->html .= '.$displayNone.';
+                    if ($option.' - '.$data == $programRoleName) {
+                        $this->html .= ' selected="selected"';
+                    }
+                    $this->html .= '>'.$data.'</option>';
                 }
-                $level1++;
+                ++$level1;
             }
-            $this->html.= '</select><br/><br/>';
-            $this->duplicateCounter++;
+            $this->html .= '</select><br/><br/>';
+            ++$this->duplicateCounter;
         }
 
 
@@ -86,46 +85,46 @@ class DuplicableDoubleDropdown extends AbstractFieldRenderer
         return $html;
     }
 
-
     private function javascriptManager()
     {
         $html = '<script type="text/javascript">';
 
-        $html.= 'DuplicateCounter = '.$this->duplicateCounter.';'."\n";
-        $html.= 'ToDuplicate=\'';
-        $html.= '<b>Program:</b> <br/> <select name="" class="DuplicableDD_level1">';
-        $html.= '<option value="">wybierz</option>';
-        $level1=1;
+        $html .= 'DuplicateCounter = '.$this->duplicateCounter.';'."\n";
+        $html .= 'ToDuplicate=\'';
+        $html .= '<b>Program:</b> <br/> <select name="" class="DuplicableDD_level1">';
+        $html .= '<option value="">wybierz</option>';
+        $level1 = 1;
         foreach ($this->dataSet as $option => $optionData) {
-            $html.='<option toChange="DuplicableDD_##NUMER##" toShow="DuplicableDD_##NUMER##_'.$level1.'" value="option'.($level1++).'">'.$option.'</option>';
+            $html .= '<option toChange="DuplicableDD_##NUMER##" toShow="DuplicableDD_##NUMER##_'.$level1.'" value="option'.($level1++).'">'.$option.'</option>';
         }
-        $html.= '</select><br/>';
+        $html .= '</select><br/>';
 
-        $html.= '<b>Rola:</b> <br/> <select name="'.$this->fieldName.'[]" class="DuplicableDD_##NUMER## DuplicableDD_level2">';
-        $html.= '<option value="">--wybierz program--</option>';
-        $level1=1;
+        $html .= '<b>Rola:</b> <br/> <select name="'.$this->fieldName.'[]" class="DuplicableDD_##NUMER## DuplicableDD_level2">';
+        $html .= '<option value="">--wybierz program--</option>';
+        $level1 = 1;
         foreach ($this->dataSet as $option => $optionData) {
-            $html.= '<option class="DuplicableDD_##NUMER##_'.$level1.'" value="">wybierz</option>';
+            $html .= '<option class="DuplicableDD_##NUMER##_'.$level1.'" value="">wybierz</option>';
             foreach ($optionData as $data) {
-                $html.='<option class="DuplicableDD_##NUMER##_'.$level1.'" value="'.$option.' - '.$data.'" style="display: none;">'.$data.'</option>';
+                $html .= '<option class="DuplicableDD_##NUMER##_'.$level1.'" value="'.$option.' - '.$data.'" style="display: none;">'.$data.'</option>';
             }
-            $level1++;
+            ++$level1;
         }
-        $html.= '</select><br/><br/>';
-        $html.= '\';'."\n\n";  # end of string
+        $html .= '</select><br/><br/>';
+        $html .= '\';'."\n\n";  // end of string
 
-        $html.= '$(".duplicatorButton").click(function(){ DuplicateCounter++;  $(".wioForms_InputFieldContainer[data-wio-forms=\''.$this->fieldName.'\']").append( ToDuplicate.replace(new RegExp(\'##NUMER##\',\'g\'),DuplicateCounter)); });'."\n";
+        $html .= '$(".duplicatorButton").click(function(){ DuplicateCounter++;  $(".wioForms_InputFieldContainer[data-wio-forms=\''.$this->fieldName.'\']").append( ToDuplicate.replace(new RegExp(\'##NUMER##\',\'g\'),DuplicateCounter)); });'."\n";
 
-        $html.= '$(".wioForms_InputFieldContainer[data-wio-forms=\''.$this->fieldName.'\']").on(\'change\',\'.DuplicableDD_level1\',function(){ ';
-        $html.= 'toChange = $(this).children("option:selected").attr("toChange"); ';
-        $html.= 'toShow = $(this).children("option:selected").attr("toShow"); ';
-        $html.= '$("."+toChange).children("option").hide(); ';
-        $html.= '$("."+toShow).show(); ';
-        $html.= '$("."+toChange).val(""); ';
-        $html.= 'console.log(toChange); console.log(toShow); ';
-        $html.= '});'."\n";
+        $html .= '$(".wioForms_InputFieldContainer[data-wio-forms=\''.$this->fieldName.'\']").on(\'change\',\'.DuplicableDD_level1\',function(){ ';
+        $html .= 'toChange = $(this).children("option:selected").attr("toChange"); ';
+        $html .= 'toShow = $(this).children("option:selected").attr("toShow"); ';
+        $html .= '$("."+toChange).children("option").hide(); ';
+        $html .= '$("."+toShow).show(); ';
+        $html .= '$("."+toChange).val(""); ';
+        $html .= 'console.log(toChange); console.log(toShow); ';
+        $html .= '});'."\n";
 
-        $html.= '</script>';
+        $html .= '</script>';
+
         return $html;
     }
 }
