d64b78a072f24387e62382f368c52d14c7b039b7
drozdza <przem.dro@gmail.com>
2016-06-28 05:03:26 -0400

    Applied fixes from StyleCI

--- src/FieldValidator/EmailNotUsed.php
+++ src/FieldValidator/EmailNotUsed.php
@@ -1,28 +1,28 @@
 <?php
+
 namespace WioForms\FieldValidator;
 
 use SuperW\Repository\Recruitment as RecruitmentRepository;
 use WioForms\FieldConverter\Stanowisko2Role;
 
-
 class EmailNotUsed extends AbstractFieldValidator
 {
-
     public function validatePHP($value, $settings)
     {
         $this->invalidMessage = 'email_already_used';
 
         global $queryBuilder;
-        $recruitmentRepository  = new RecruitmentRepository($queryBuilder);
-        $roleConverter          = new Stanowisko2Role();
+        $recruitmentRepository = new RecruitmentRepository($queryBuilder);
+        $roleConverter = new Stanowisko2Role();
 
-        $role       = $roleConverter->convert($this->wioForms->entryData['stanowisko']);
-        $program    = $this->wioForms->entryData['akcja'];
+        $role = $roleConverter->convert($this->wioForms->entryData['stanowisko']);
+        $program = $this->wioForms->entryData['akcja'];
 
         $this->valid = !$recruitmentRepository
                             ->ifEmailAlreadyUsed($value, $program, $role);
 
         $this->setAnswer();
+
         return $this->getReturn();
     }
 }
