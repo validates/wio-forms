e5cfe1c505a3f483fe9e0cc2002a27344be9c9fb
Przemysław Drożdża <przemyslaw.drozdza@wiosna.org.pl>
2016-06-28 12:21:52 +0200

    added RadioList

--- src/FieldRenderer/RadioList.php
+++ src/FieldRenderer/RadioList.php
@@ -7,20 +7,21 @@ class RadioList extends AbstractFieldRenderer
     public function showToEdit()
     {
         $this->html = '';
-        $this->wioForms->headerCollectorService->addJS('assets/js/jquery-1.12.3.min.js');
-        $this->wioForms->headerCollectorService->addJS('assets/js/jquery-ui.min.js');
-        $this->wioForms->headerCollectorService->addJS('assets/js/jquery-ui.datepicker-pl.js');
-        $this->wioForms->headerCollectorService->addCss('assets/jquery-ui.min.css');
-        $this->wioForms->headerCollectorService->addCss('assets/jquery-ui.structure.min.css');
-        $this->wioForms->headerCollectorService->addCss('assets/jquery-ui.theme.min.css');
-
+        $this->prepareDataSet();
         $this->inputContainerHead();
         $this->standardErrorDisplay();
         $this->inputTitleContainer();
         $this->inputFieldContainerHead();
 
-        $this->html .= '<script type="text/javascript">$(function(){ $(".datePicker_'.$this->fieldName.'").datepicker({dateFormat: \'yy-mm-dd\', changeMonth: true, changeYear: true, yearRange: \'1940:2000\'}); });</script>';
-        $this->html .= '<input type="text" name="'.$this->fieldName.'" class="datePicker_'.$this->fieldName.'" value="'.$this->value.'" />';
+        $this->html .= '<ul class="wioForms_RadioList">';
+        foreach ($this->dataSet as $option => $option_name) {
+            $checked = '';
+            if ($option == $this->value) {
+                $checked = ' checked="checked"';
+            }
+            $this->html .= '<li><input type="checkbox" value="'.$option.'"'.$checked.' name="'.$this->fieldName.'">'.$option_name.'</li>';
+        }
+        $this->html .= '</ul>';
 
         $this->inputFieldContainerTail();
         $this->inputContainerTail();
