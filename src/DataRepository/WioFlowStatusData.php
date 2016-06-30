2a18d8cba52a278b381780e8e3d21a3135a1b5f0
Daniel Skowroński <daniel@dsinf.net>
2016-06-30 17:07:22 +0200

    status flow

--- src/DataRepository/WioFlowStatusData.php
+++ src/DataRepository/WioFlowStatusData.php
@@ -6,10 +6,22 @@ class WioFlowStatusData extends AbstractDataRepository
 {
     public function getData($requiredFields)
     {
+        $curr_status = $requiredFields['status']!==false ? $requiredFields['status'] :  1;
+
         global $queryBuilder;
         $statusList = $queryBuilder->table('wio_flow_status_names')
-            ->select('status_id', 'name')
+            ->select('status_id', 'name', 'allowed_next')
             ->where('wio_flow_id', '=', 1)
+            ->where('status_id', '=', $curr_status)
+            ->first();
+
+        $allowed_next = $statusList->allowed_next.",".$curr_status;
+        $allowed_next = explode(",", $allowed_next);
+
+        $statusList = $queryBuilder->table('wio_flow_status_names')
+            ->select('status_id', 'name', 'allowed_next')
+            ->where('wio_flow_id', '=', 1)
+            ->whereIn('status_id', $allowed_next)
             ->get();
 
         foreach ($statusList as $status) {
