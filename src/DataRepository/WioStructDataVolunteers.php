6f6c3c49a2c3557385cb0b66dac67270412f4c45
Przemek <przem.dro@gmail.com>
2016-08-10 16:47:00 +0200

    rozdzielenie wyswietlania rejonow na mapach liderow i wolontariuszy

---
+++ src/DataRepository/WioStructDataVolunteers.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace WioForms\DataRepository;
+
+use WioStruct\Core\StructDefinition;
+use WioStruct\WioStruct;
+
+class WioStructDataVolunteers extends AbstractDataRepository
+{
+    public function getData($requiredFields)
+    {
+        $this->data = [];
+
+        $wioStruct = new WioStruct(new \Pixie\QueryBuilder\QueryBuilderHandler());
+
+        $woj = $wioStruct->structQuery(
+            (new StructDefinition())
+                ->networkName('administrative')
+                ->nodeTypeName('state')
+            )
+            ->get('Node');
+
+        $wojewodztwa = [];
+        foreach ($woj as $w) {
+            $wojewodztwa[$w->NodeName] =
+            [
+                'node_id' => $w->NodeId,
+                'lat' => $w->NodeLat,
+                'lng' => $w->NodeLng,
+                'szp_regions' => [],
+                'ap_cities' => [],
+                'ap_collegium' => [],
+            ];
+        }
+
+
+        $szp_regions = $wioStruct->structQuery(
+            (new StructDefinition())
+                ->networkName('Szlachetna Paczka')
+                ->nodeTypeName('rejon')
+                ->flagTypeName('mapa_liderow_2016_rejon_szp')
+                ->linkParent(
+                    (new StructDefinition())
+                        ->networkName('administrative')
+                        ->nodeTypeName('state')
+                )
+            )
+            ->get('Node');
+
+        usort($szp_regions, function ($a, $b) {
+            return strcmp($a->NodeName, $b->NodeName);
+        });
+        foreach ($szp_regions as $region) {
+            $wojewodztwa[$region->ParentNodeName]['szp_regions'][$region->NodeName] = [
+                'node_id' => $region->NodeId,
+                'lat' => $region->NodeLat,
+                'lng' => $region->NodeLng,
+            ];
+        }
+
+        $ap_cities = $wioStruct->structQuery(
+            (new StructDefinition())
+                ->networkName('administrative')
+                ->nodeTypeName('city')
+                ->flagTypeName('mapa_liderow_2016_miasto_ap')
+                ->linkParent(
+                    (new StructDefinition())
+                        ->networkName('administrative')
+                        ->nodeTypeName('state')
+                )
+            )
+            ->get('Node');
+        foreach ($ap_cities as $city) {
+            $wojewodztwa[$city->ParentNodeName]['ap_cities'][$city->NodeName] = [
+                'node_id' => $city->NodeId,
+                'lat' => $city->NodeLat,
+                'lng' => $city->NodeLng,
+            ];
+        }
+
+        $apCollegiumList = $wioStruct->structQuery(
+            (new StructDefinition())
+                ->networkName('Akademia Przyszłości')
+                ->nodeTypeName('kolegium')
+                ->flagTypeName('mapa_wolontariuszy_2016')
+                ->linkParent(
+                    (new StructDefinition())
+                        ->networkName('administrative')
+                        ->nodeTypeName('state')
+                )
+            )
+            ->get('Node');
+
+        foreach ($apCollegiumList as $apCollegium) {
+            $wojewodztwa[$apCollegium->ParentNodeName]['ap_collegium'][$apCollegium->NodeName] = [
+                'node_id' => $apCollegium->NodeId,
+                'lat' => $apCollegium->NodeLat,
+                'lng' => $apCollegium->NodeLng,
+            ];
+        }
+
+        $this->data = $wojewodztwa;
+
+
+        if (empty($this->data)) {
+            $this->repositoryDefinition['success'] = false;
+            $this->repositoryDefinition['message'] = 'no_map_data';
+            $this->data = false;
+        } else {
+            $this->repositoryDefinition['success'] = true;
+        }
+        $this->repositoryDefinition['tried'] = true;
+
+        return $this->data;
+    }
+}
